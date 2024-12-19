a1962d3c6f9a724940c2d467c228076072d25c8f
Jun Pataleta <jun@moodle.com>
2024-12-19 18:52:47 +0800

    NOBUG: Main version bump

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2024101600.00;              // YYYYMMDD = weekly release date of this DEV branch.
+$version  = 2024101600.01;              // YYYYMMDD = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 $release  = '4.6rc1 (Build: 20241219)'; // Human-friendly version name
