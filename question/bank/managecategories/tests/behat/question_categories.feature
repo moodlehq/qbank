cwarwicker/moodle

MDL-84578 qbank_managecategories: Add category validation.
- This adds simple logic check to filter.js to stop user
submitting an invalid category.
- This adds new behat steps to check this validation.
- This adds a new behat test to test this in managecategories.

question/bank/managecategories/tests/behat/question_categories.feature
@@ -132,3 +132,20 @@ Feature: A teacher can put questions in categories in the question bank
     And I open the "last" add to quiz menu
     And I follow "from question bank"
     And the field "Also show questions from subcategories" matches value "0"
+
+  Scenario: Filter question by an invalid category should show validation error
+    When I am on the "Course 1" "core_question > course question bank" page
+    And I click on "Default for C1" "text" in the ".form-autocomplete-selection" "css_element"
+    And I click on "Apply filters" "button"
+    Then The "Category" field validation check should return "false"
+    And The "Category" field validation message should contain "You must select a valid category"
+
+  Scenario: Correcting an invalid category should no longer show validation error
+    When I am on the "Course 1" "core_question > course question bank" page
+    # First try to submit with a blank category.
+    And I click on "Default for C1" "text" in the ".form-autocomplete-selection" "css_element"
+    And I click on "Apply filters" "button"
+    # Then apply a correct category.
+    And I apply question bank filter "Category" with value "Used category"
+    Then The "Category" field validation check should return "true"
+    And I should see "Test question to be moved"

moodlehq/qbank

question/bank/managecategories/tests/behat/question_categories.feature
@@ -137,3 +137,20 @@ Feature: A teacher can put questions in categories in the question bank
     And I open the "last" add to quiz menu
     And I follow "from question bank"
     And the field "Also show questions from subcategories" matches value "0"
+
+  Scenario: Filter question by an invalid category should show validation error
+    When I am on the "Course 1" "core_question > course question bank" page
+    And I click on "Default for C1" "text" in the ".form-autocomplete-selection" "css_element"
+    And I click on "Apply filters" "button"
+    Then the "Category" field validity check should return "false"
+    And the "Category" field validation message should contain "You must select a valid category"
+
+  Scenario: Correcting an invalid category should no longer show validation error
+    When I am on the "Course 1" "core_question > course question bank" page
+    # First try to submit with a blank category.
+    And I click on "Default for C1" "text" in the ".form-autocomplete-selection" "css_element"
+    And I click on "Apply filters" "button"
+    # Then apply a correct category.
+    And I apply question bank filter "Category" with value "Used category"
+    Then the "Category" field validity check should return "true"
+    And I should see "Test question to be moved"